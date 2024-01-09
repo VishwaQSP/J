28362dda4d26e53260e98c7da22cfe99d9d47928
Admin <Admin@DESKTOP-BK8R38T>
2024-01-09 11:54:27 +0530

    sxg

---
+++ J/src/test/java/test/Tc2.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc2 {
+	
+	@Test
+	public void tc2()
+	{
+		Reporter.log("Tc2 Ruuning",true);
+		Reporter.log("Tc2 testing",true);
+		
+	}
+
+}
---
+++ J/src/test/java/test/Tc3Test.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc3Test {
+	@Test
+	public void tc3()
+	{
+		Reporter.log("Tc3 Passed",true);
+		Reporter.log("Tc3A Passed",true);
+		Reporter.log("Tc3B Passed",true);
+		
+	}
+
+}
